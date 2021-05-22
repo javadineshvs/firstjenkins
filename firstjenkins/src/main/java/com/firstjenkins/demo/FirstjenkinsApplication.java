a521bae2d4b9abfaa907801e1925ff47d999a51d
javadineshvs <javadineshvs@gmail.com>
2021-05-23 00:58:10 +0530

    initial commit

---
+++ firstjenkins/src/main/java/com/firstjenkins/demo/FirstjenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.firstjenkins.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstjenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstjenkinsApplication.class, args);
+	}
+
+}
---
+++ firstjenkins/src/test/java/com/firstjenkins/demo/FirstjenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstjenkins.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstjenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
